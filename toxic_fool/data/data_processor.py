0d119ca7bdeafd69a9156652a470bf991a1e6750
orgoro <or.gorodissky@d-id.com>
2018-06-23 14:46:56 +0300

    data_processor: padd and dump added

--- toxic_fool/data/data_processor.py
+++ toxic_fool/data/data_processor.py
@@ -7,22 +7,26 @@ import pandas as pd
 import numpy as np
 import re
 import unicodedata
+from os import path
+import pickle
 
 import resources as res
+import resources_out as out
 
 
 class DataProcessor(object):
 
-    def __init__(self, train_d=res.TRAIN_CSV_PATH, test_d=res.TEST_CSV_PATH, clean_text=True):
+    def __init__(self, train_d=res.TRAIN_CSV_PATH, test_d=res.TEST_CSV_PATH, clean_text=True, pad_seq=True):
         # type: (str, str, str) -> None
         self._train_d = pd.read_csv(train_d)
         self._test_d = pd.read_csv(test_d)
         # self._test_l = pd.read_csv(test_l)
+        self._max_seq_len = 1000
         self._tokenizer = text.Tokenizer(char_level=True, lower=True)  # TODO: max number of words
-        self._max_seq_len = 1200
 
         self.processed = False  # True after data processing
         self._clean_words = clean_text
+        self._pad_seq = pad_seq
         self.classes = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
         self.seq_train = None  # type: np.ndarray
         self.seq_val = None  # type: np.ndarray
@@ -50,6 +54,7 @@ class DataProcessor(object):
         print('done fitting! unique tokens found: {}'.format(len(self._tokenizer.word_index.keys())))
 
         n_elem = len(text_train)
+        np.random.seed(42)
         indices = np.random.permutation(n_elem)
         thresh = n_elem // 10
 
@@ -63,6 +68,12 @@ class DataProcessor(object):
         self.seq_train = self._tokenizer.texts_to_sequences(text_train[train_idx])
         self.seq_val = self._tokenizer.texts_to_sequences(text_train[val_idx])
         self.seq_test = self._tokenizer.texts_to_sequences(text_test)
+
+        if self._pad_seq:
+            self.seq_train = sequence.pad_sequences(sequences=self.seq_train, maxlen=self._max_seq_len)
+            self.seq_val = sequence.pad_sequences(sequences=self.seq_val, maxlen=self._max_seq_len)
+            self.seq_test = sequence.pad_sequences(sequences=self.seq_test, maxlen=self._max_seq_len)
+
         self.processed = True
 
         print('processing done! sizes: train {} | val {} | test {}'.format(len(self.seq_train),
@@ -72,13 +83,20 @@ class DataProcessor(object):
     def get_tokens(self):
         return self._tokenizer.word_index.keys()
 
+    def dump_seq(self):
+        print('saving sequences to: ', out.RES_OUT_DIR)
+        np.save(path.join(out.RES_OUT_DIR, 'seq_train.npy'), self.seq_train)
+        np.save(path.join(out.RES_OUT_DIR, 'seq_val.npy'), self.seq_val)
+        np.save(path.join(out.RES_OUT_DIR, 'seq_test.npy'), self.seq_test)
+
 
 def example():
     data_pro = DataProcessor()
     data_pro.process_data()
     print('tokens: {}'.format(list(data_pro.get_tokens())))
-    print('first sequence: {}'.format(data_pro.seq_train[0]))
+    print('first sequence: {} \n{}'.format(data_pro.seq_train[0].shape, data_pro.seq_train[0]))
     print('first label: {}'.format(data_pro.labels_train[0]))
+    data_pro.dump_seq()
 
 
 if __name__ == '__main__':
---
+++ toxic_fool/resources_out/__init__.py
@@ -0,0 +1,3 @@
+from os import path
+
+RES_OUT_DIR = path.dirname(path.abspath(__file__))
