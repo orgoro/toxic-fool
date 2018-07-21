ff216856aa1bef5640a16c788ccdc7a859845b15
orgoro <or.gorodissky@d-id.com>
2018-07-21 14:43:30 +0300

    agent: added base class

---
+++ toxic_fool/agents/agent.py
@@ -0,0 +1,50 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import abc
+import numpy as np
+
+from models import ToxicityClassifier
+
+
+class AgentConfig(object):
+
+    def __init__(self):
+        # should be called after child class init
+        self.vars = vars(self)
+
+    def __str__(self):
+        print('________________________')
+        for k, v in self.vars.items():
+            print('|{:10} | {:10}|'.format(k, v))
+        print('________________________')
+
+
+class Agent(object):
+
+    def __init__(self,  tox_model, config):
+        # type: (ToxicityClassifier, AgentConfig) -> None
+        self._config = config
+        self._tox_model = tox_model
+
+    @abc.abstractmethod
+    def _build_graph(self):
+        pass
+
+    @abc.abstractmethod
+    def _train_step(self):
+        pass
+
+    @abc.abstractmethod
+    def train(self):
+        pass
+
+    @abc.abstractmethod
+    def restore(self, restore_path):
+        pass
+
+    @abc.abstractmethod
+    def attack(self, seq, target_confidence):
+        pass
+
