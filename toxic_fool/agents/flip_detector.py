70a965b249d38efb1121516d5adc4246adbce55a
orgoro <or.gorodissky@d-id.com>
2018-10-27 18:59:05 +0300

    flip-detector: pylint disable

--- toxic_fool/agents/flip_detector.py
+++ toxic_fool/agents/flip_detector.py
@@ -218,6 +218,7 @@ class FlipDetector(Agent):
         batch_size = self._config.batch_size
         offset = batch_num * batch_size
         dataset = dataset
+        # pylint: disable=unused-variable
 
         if not validation:
             replace_chars = np.ones(shape=(batch_size, self._config.embedding_shape[0]))
